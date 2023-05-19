dbe794a45ada1ec6c8385284330c12a7a2191d2b
Waqas Ali <waqas_111@hotmail.com>
2023-05-19 11:05:18 +0500

    loops

--- index.php
+++ index.php
@@ -13,17 +13,17 @@ $x = 1;
 $y = 1;
 
 $x = $x + $y;
-echo $x;
+// echo $x;
 
-echo "<br>";
+// echo "<br>";
 
 $x = 1;
 $y = 1;
 
 $x *= $y;
 
-echo $x;
-exit;
+// echo $x;
+
 
 
 /*
@@ -41,37 +41,82 @@ PHP is a loosely typed language:
 */
 
 $unit_price = 16;
-echo "<br>";
+// echo "<br>";
 
 
-var_dump($unit_price);
+// var_dump($unit_price);
 // $quantity = 1;
 
 
 $unit_price = 'a';
 
-echo "<br>";
-var_dump($unit_price);
+// echo "<br>";
+// var_dump($unit_price);
 
 $unit_price = 10.9;
 $unit_price = 10.9;
 // echo "<br>"
 
-var_dump($unit_price);
+// var_dump($unit_price);
 
 $fname = "Waqas";
 $midname = "Ali";
 $lastname = "Azhar";
 
-echo "<p>" . $fname . '  &nbsp;&nbsp;  ' . $midname . ' &nbsp;&nbsp;   ' . $lastname . "</p>";
+// echo "<p>" . $fname . '  &nbsp;&nbsp;  ' . $midname . ' &nbsp;&nbsp;   ' . $lastname . "</p>";
 
-echo 10 . 9;
+// echo 10 . 9;
 
+$numberOne = 29;
+$numberTwo = 2;
 
+for ($i = 0; $i < 25; ++$i) {
+    echo "*";
+    echo "<br>";
+}
 
 
 // $total = $unit_price * $quantity;
 // echo "<br>";
 // print_r($total);
+
+
+/**
+ * 
+ * Please write a computer program that can caluculate values of 
+ * N number of students where each student's Maths marks are given.
+ * 
+ * Provided that N is an integer whose values are
+ *   0 < N < 10
+ *   marks are 4,5,33,66,88,11,22,33,44,55
+ * 
+ *  Array: an array is a collection of memory cells where you store similar records
+ *  in consecutive locations where arrays first element's address is 0 and last element's
+ *  adress is Length - 1;
+ */
+
+
+
+$marks = array(4, 5, 33, 66, 88, 11, 22, 33, 44, 55);
+$lengthOfArray = count($marks);
+
+for ($i = 0; $i < $lengthOfArray; ++$i) {
+    echo "Marks for Student " . $i . " are " . $marks[$i];
+    echo "<br>";
+}
+
+
+$marks = array(array(30, 40), array(20, 45), array(90, 99), array(10, 20), array(11, 33), array(32, 47), array(31, 89), array(44, 54), array(55, 66), array(65, 87));
+$lengthOfArray = count($marks);
+
+for ($i = 0; $i < $lengthOfArray; ++$i) {
+    echo "Marks for maths for Student " . $i . " are " . $marks[$i][0] . "       Marks for english Student " . $i . " are " . $marks[$i][1];
+    echo "<br>";
+}
+
+print_r('loop is ended');
+
+
+
 
 ?>
