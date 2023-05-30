5a1f86bf00c8de298fafdee0235a39ee6b0e1451
Waqas Ali <waqas_111@hotmail.com>
2023-05-30 11:16:15 +0500

    db connection

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,8 @@ $username = '';
 if (!empty($_GET['username'])) {
     $username = $_GET['username'];
 }
+
+include 'config/db.php';
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
